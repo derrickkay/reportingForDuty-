3cc39cf7a3a57fbc656e6c8b0040e364652fb033
Derrick Kay <82616888+derrickkay@users.noreply.github.com>
2022-02-05 20:36:16 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+//function here:
+//function here:
+const reportingForDuty = (rank, lastName) => `${rank} ${lastName} reporting for duty!`
+
+
+
+ console.log(reportingForDuty('Manager', 'Kay')) // Should return '***** ***** reporting for duty!'
+
+// add more function calls of your o
+
+/*
+Using string concatenation:
+const reportingForDuty = (rank, lastName) => rank + " " + lastName + " " + "reporting for duty!"
+
+As a function declaration:
+function reportingForDuty(rank, lastName) {
+    return `${rank} ${lastName} reporting for duty!`
+}
+*/
